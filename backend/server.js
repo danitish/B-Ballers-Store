3dc3c268fd384308b78cf735a982ab4ca3019698
danitish <97957690+danitish@users.noreply.github.com>
2022-12-12 11:52:25 +0000

    Static serve update

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,8 @@ connectDB();
 const app = express();
 const __dirname = path.resolve();
 
+console.log(path.resolve());
+
 app.use(express.json());
 app.use(cors());
 if (process.env.NODE_ENV === "development") {
@@ -40,9 +42,9 @@ app.get("/api/config/paypal", (req, res) =>
 app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,"..", "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname,"frontend", "build", "index.html"));
   });
 }
 app.use(notFound);
